15872fe21e0a79ae365a8431204dd5d70697cdc5
Matheus <matheus.sansonowicz@ufn.edu.br>
2025-06-02 19:45:37 -0300

    fComit

---
+++ src/test/java/com/example/atividadepraticaaula12/AtividadePraticaAula12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atividadepraticaaula12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadePraticaAula12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
